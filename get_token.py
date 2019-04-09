8f359b6f9433ed5475defb3726598fcf56512f8a
Blake Hartshorn <contact@blakehartshorn.com>
2019-04-08 22:06:37 -0400

    Added script to fetch token.

---
+++ get_token.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+import requests,argparse,sys,json
+
+'''
+This will need to be re-run when the OAuth token expires.
+
+If you need help getting started with the Nest API, see
+this document from Google:
+https://codelabs.developers.google.com/codelabs/wwn-api-quickstart/
+
+Go to the "Authorization URL" from the OAuth client overview
+in the Nest developer portal, follow the link and sign in
+to your Nest account. The needed PIN will be presented there.
+'''
+
+parser = argparse.ArgumentParser(description="Setup OAuth token for Nest user.")
+parser.add_argument("-i",metavar="client_id",required=True,help="Client ID for OAuth.")
+parser.add_argument("-s",metavar="secret",required=True,help="Client Secret for OAuth.")
+parser.add_argument("-p",metavar="pin",required=True,help="PIN Authorization for account.")
+parser.add_argument("-n",metavar="name",help="Friendly name.")
+args = parser.parse_args()
+
+if args.n:
+    filename = f"{args.n}_token.json"
+else:
+    filename = "nest_token.json"
+
+payload = {
+    'client_id': args.i,
+    'client_secret': args.s,
+    'grant_type': 'authorization_code',
+    'code': args.p
+}
+
+r = requests.post("https://api.home.nest.com/oauth2/access_token",data=payload)
+if r.status_code != 200:
+    print("Incorrect status code returned from server.")
+    print(r.status_code)
+    print(r.content)
+    sys.exit(3)
+
+with open(filename,"w") as f:
+    f.write(json.dumps(r.json()))
+
+print(f"{filename} written successfully.")
+    
